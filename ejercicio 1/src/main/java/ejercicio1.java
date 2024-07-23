55834477d09efc853d29d9622fd6e360f4523185
juban1234 <alexzo8677gmail.com>
2024-07-23 16:44:31 -0500

    ejercicio 1.1

--- ejercicio 1/src/main/java/ejercicio1.java
+++ ejercicio 1/src/main/java/ejercicio1.java
@@ -2,6 +2,17 @@
 public class ejercicio1 {
     public static void main(String[] args) {
         // tipos de datos 
+        byte a = 127; //numero entero con signo
+        short b = 30000; //numero entero con signo
+        int c = 1000000000; //numero entero con signo
+        long d = 1000000000; //numero entero con signo
+        
+        float e = 30004; // numero con coma flotante
+        double f = 100000000; // numero con coma flotante
+        
+        char g = 'a'; //caracter unico
+        
+        boolean h = true; //dato logico
     }
     
 }
